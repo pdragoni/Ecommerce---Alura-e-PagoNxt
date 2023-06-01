e31a0516d0404f7548c82c74f9b082b0c6c0a830
Ferreira Ferreira Paulo Henrique Barros Faria Dragoni <n134441@gruposantander.com>
2023-06-01 18:38:35 -0300

    [feat] lista produtos por preço de acordo com filtro e projeção

---
+++ mongo/listaProdutosPorPreco.js
@@ -0,0 +1,8 @@
+use("mongo-ecomm")
+
+const filter = { preco: { $gte: 1000, $lte: 2000 }};
+const projection = {_id:1, nome: 1, preco: 1};
+
+const filteredProducts = db.products.find(filter, projection);
+
+console.log(filteredProducts);
