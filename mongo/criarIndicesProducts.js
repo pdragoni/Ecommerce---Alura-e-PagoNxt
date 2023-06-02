a2f5e074ea373bc4ea046abef4255285913d56b5
pdragoni <n134441@gruposantander.com>
2023-06-02 13:15:29 -0300

    [entrega semana1-docker-mongodb]

--- mongo/criarIndicesProducts.js
+++ mongo/criarIndicesProducts.js
@@ -1,13 +1,24 @@
-const indexCreation = db.products.createIndex(
+const categoryIdCreation = db.products.createIndex(
   {
-    "nome": 1
+    "categorias": 1
   },
   {
     unique: true,
     sparse: true,
     expireAfterSeconds: 3600,
-    name: "product_id"
+    name: "category_id"
   }
 )
-console.log(indexCreation);
+console.log(categoryIdCreation);
+
+const productIdCreation = db.products.createIndex({
+  "nome": 1
+}, {
+  unique: true,
+  sparse: true,
+  expireAfterSeconds: 3600,
+  name: "product_id"
+})
+console.log(productIdCreation);
+// console.log(db.products.find());
 console.log('Fim do arquivo')
