69a1cf856defa77341760dda801b82a56488cb3e
pdragoni <n134441@gruposantander.com>
2023-06-08 13:45:15 -0300

    [fix] 'insereProdutos'

--- mongo/insereProdutos.js
+++ mongo/insereProdutos.js
@@ -1,5 +1,7 @@
 const newProducts = require('./ecommProdutos.json');
 
+use("mongo-ecomm");
+
 db.products.drop(function (err, delOK) {
   if (err) throw err;
   if (delOK) console.log("Collection deleted");
