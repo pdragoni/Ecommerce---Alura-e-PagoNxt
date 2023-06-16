4a025d796060cf80aad8a4f981ab48dead669b0d
pdragoni <n134441@gruposantander.com>
2023-06-16 11:10:51 -0300

    [fix] implementação case --recuperarCategoriaPorId

--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,20 +1,23 @@
-import { findCategories, findCategoryById } from "./CategoryService";
+import { CategoryService } from "./CategoryService.js";
+const { findCategories, findCategoryById } = CategoryService;
 
-const args = process.argv /
+const args = process.argv;
 
-  async function processarComando() {
-    console.log(args);
-    switch (args[2]) {
-      case "--listarCategorias":
-        console.log('--listar', args)
-        const categories = await findCategories()
-        return categories;
-      case "--recuperarCategoriaPorId":
-        console.log('--porId', args)
-      // return findCategoryById();
-      default:
-        console.log("default", args);
-    }
-  };
+async function processarComando() {
+  // console.log(args);
+  switch (args[2]) {
+    case "--listarCategorias":
+      // console.log('--listar', args)
+      const categories = await findCategories()
+      return categories;
+    case "--recuperarCategoriaPorId":
+      const id = args[3];
+      // console.log('--porId', args)
+      const category = await findCategoryById(id);
+      return category;
+    default:
+      console.log("default", args);
+  }
+};
 
-processarComando(args);
+processarComando();
