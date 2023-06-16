b92f6909b91ced9e586819fef78b1c19b23aa576
pdragoni <n134441@gruposantander.com>
2023-06-16 10:43:35 -0300

    [feat] Transforma CategoryService em uma classe com métodos estáticos para encontrar categorias e categoria por id

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -1,20 +1,34 @@
-async function retrieveStatusResponse() {
-  const url = "http://localhost:3000/categories";
-  try {
-    const response = await fetch(url)
-    const { status } = response;
-    const responseJson = await response.json();
+export class CategoryService {
+  static async findCategories() {
+    const url = "http://localhost:3000/categories";
+    try {
+      const response = await fetch(url)
+      const { status } = response;
+      const responseJson = await response.json();
 
-    // const resposta = `response status ${status} \n ${categories}`;
-    // console.log(resposta);
+      // const resposta = `response status ${status} \n ${categories}`;
+      // console.log(resposta);
 
-    console.log("status response", status);
-    console.log(responseJson);
+      console.log("status response", status);
+      console.log(responseJson);
+    }
+    catch (err) {
+      console.log(err);
+    }
   }
-  catch (err) {
-    console.log(err);
-  }
-};
-// retrieveStatusResponse();
 
-export default retrieveStatusResponse;
+  static async findCategoryById(id) {
+    const url = "http://localhost:3000/categories";
+    try {
+      const response = await fetch(url)
+      const { status } = response;
+      const responseJson = await response.json();
+      const category = await responseJson.filter((cat => cat.id === { id }))
+      category ? console.log("response status:", status, category) : console.log("Category Not Found");
+      console.log(category);
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+}
